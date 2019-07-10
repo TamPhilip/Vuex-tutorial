69ec1b680d10ad1c3a3bbcef24fb4e1edaa8ccb9
TamPhilip <philip.tam.code@gmail.com>
2019-07-10 10:28:48 -0400

    Done

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <product-list-one> 
+      
+    </product-list-one>
+    <product-list-two> 
+
+    </product-list-two>
+  </div>
+</template>
+
+<script>
+import ProductListOne from './components/ProductListOne'
+import ProductListTwo from './components/ProductListTwo'
+export default {
+  name: 'app',
+  data () {
+    return {
+      
+    }
+  }, components: {
+    'product-list-one': ProductListOne,
+    'product-list-two': ProductListTwo
+  }
+}
+</script>
+
+<style>
+body {
+  font-family: 'Helvetica-Light';
+  color: #555;
+}
+</style>
